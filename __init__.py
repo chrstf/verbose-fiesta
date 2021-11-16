7af39b2c847ac5b52a3b7dab87de0e3b03497506
Christophe <christophe@imada.sdu.dk>
2021-11-16 14:58:46 +0100

    renamed module

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .verbosefiesta import message, warning_m, error_m, locator, bool_color
-from .verbosefiesta.progressbar import progress_bar
+from .babble import message, warning_m, error_m, locator, bool_color
+from .babble.progressbar import progress_bar
